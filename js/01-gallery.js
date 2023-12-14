a48370c18f3a38bb6f7feb5387d027e3fc763ca6
Eduardo José Maya Rodriguez <36246464+edopore@users.noreply.github.com>
2023-12-14 14:38:44 -0500

    UPDATE se realizan tareas 1 y 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const newImages = [...galleryItems]
+  .map(
+    (galleryItem) =>
+      `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="large-image.jpg"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", newImages);
+
+const images = document.querySelectorAll(".gallery__item");
+
+images.forEach((image) => {
+  image.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const instance = createImage(image.children[0]["href"]);
+    instance.show();
+
+    document.addEventListener("keydown", (event) => {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+    });
+  });
+});
+
+function createImage(link) {
+  return basicLightbox.create(`
+    <img src="${link}">
+`);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+let gallery = document.querySelector(".gallery");
+
+const newImages = [...galleryItems]
+  .map(
+    (galleryItem) =>
+      `
+<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", newImages);
+
+gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {});
+
+gallery.on("error.simplelightbox", function (e) {
+  console.log(e); // Some usefull information
+});
